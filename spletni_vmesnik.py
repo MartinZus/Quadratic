5d6b226b3448bfd82edb4cd158d38e63137538b5
Martin Žust <martin.zust@student.fmf.uni-lj.si>
2021-08-20 23:04:01 +0200

    commit

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -6,8 +6,18 @@ def server_static(filepath):
     return bottle.static_file(filepath, root='static')
 
 @bottle.get("/")
+def izbira_jezika():
+    return bottle.template("jeziki.html")
+
+@bottle.get("/prva_stran/")
 def prvi_zaslon():
-    return bottle.template("prva_stran.html")
+    global jezik
+    jezik = bottle.request.query["jezik"]
+    return bottle.template("prva_stran.html", jezik=jezik)
+
+@bottle.get("/prva_stran1/")
+def zaslon():
+    return bottle.template("prva_stran.html", jezik=jezik)
 
 @bottle.get("/quadratic/")
 def igralni_zaslon():
@@ -21,20 +31,20 @@ def igralni_zaslon():
     m = 0
     global velikost
     velikost = bottle.request.query["velikost"]
-    if int(velikost) not in range(3, 9):
+    if int(velikost) not in range(3, 6):
         n = 1
     znesek = bottle.request.query["znesek"]
     if int(znesek) not in range(200, 5001):
         m = 1
     if m != 0 or n != 0:
-        return bottle.template("prijava.html", n=n, m=m)
+        return bottle.template("prijava.html", n=n, m=m, jezik=jezik)
     global quadratic
     quadratic = Quadratic(int(velikost), ime1, ime2, int(znesek), int(znesek))
-    return bottle.template("quadratic.html", ime1= quadratic.igralec1, ime2=quadratic.igralec2, znesek1=quadratic.denar1, znesek2=quadratic.denar2, velikost=quadratic.velikost)
+    return bottle.template("quadratic.html", ime1= quadratic.igralec1, ime2=quadratic.igralec2, znesek1=quadratic.denar1, znesek2=quadratic.denar2, velikost=quadratic.velikost, jezik=jezik)
 
 @bottle.get("/prijava/")
 def prijavljanje():
-    return bottle.template("prijava.html", n=0, m=0)
+    return bottle.template("prijava.html", n=0, m=0, jezik=jezik)
 
 @bottle.get("/igra1/")
 def igrica1():
@@ -52,13 +62,13 @@ def igrica1():
                 stave.append((n, m, int(kolicina)))
                 vsota += int(kolicina)
     if len(stave) != int(velikost):
-        return bottle.template("stave1.html", ime1=ime1, velikost=int(velikost), n=1)
+        return bottle.template("stave1.html", ime1=ime1, velikost=int(velikost), n=1, jezik=jezik)
     elif vsota >= quadratic.denar1:
-        return bottle.template("stave1.html", ime1=ime1, velikost=int(velikost), n=2)
+        return bottle.template("stave1.html", ime1=ime1, velikost=int(velikost), n=2, jezik=jezik)
     global igra1
     igra1 = Igra(int(velikost), quadratic.denar1, quadratic.denar2, stave)
     igra1.polog()
-    return bottle.template("igra1.html", ime1=ime1, ime2=ime2, znesek1=igra1.denar1, znesek2=igra1.denar2, velikost=int(velikost))
+    return bottle.template("igra1.html", ime1=ime1, ime2=ime2, znesek1=igra1.denar1, znesek2=igra1.denar2, velikost=int(velikost), jezik=jezik)
 
 @bottle.get("/igra2/")
 def igrica2():
@@ -75,21 +85,21 @@ def igrica2():
                 stave.append((n, m, int(kolicina)))
                 vsota += int(kolicina)
     if len(stave) != int(velikost):
-        return bottle.template("stave2.html", ime2=ime2, velikost=int(velikost), n=1)
+        return bottle.template("stave2.html", ime2=ime2, velikost=int(velikost), n=1, jezik=jezik)
     elif vsota >= quadratic.denar2:
-        return bottle.template("stave2.html", ime2=ime2, velikost=int(velikost), n=2)
+        return bottle.template("stave2.html", ime2=ime2, velikost=int(velikost), n=2, jezik=jezik)
     global igra2
     igra2 = Igra(int(velikost), quadratic.denar2, quadratic.denar1, stave)
     igra2.polog()
-    return bottle.template("igra2.html", ime1=ime1, ime2=ime2, znesek2=igra2.denar1, znesek1=igra2.denar2, velikost=int(velikost))
+    return bottle.template("igra2.html", ime1=ime1, ime2=ime2, znesek2=igra2.denar1, znesek1=igra2.denar2, velikost=int(velikost), jezik=jezik)
 
 @bottle.get("/stave1/")
 def stava1():
-    return bottle.template("stave1.html", ime1=ime1, velikost=int(velikost), n=0)
+    return bottle.template("stave1.html", ime1=ime1, velikost=int(velikost), n=0, jezik=jezik)
 
 @bottle.get("/stave2/")
 def stava2():
-    return bottle.template("stave2.html", ime2=ime2, velikost=int(velikost), n=0)
+    return bottle.template("stave2.html", ime2=ime2, velikost=int(velikost), n=0, jezik=jezik)
 
 @bottle.get("/igralnica1/")
 def gibanje11():
@@ -97,7 +107,7 @@ def gibanje11():
     igra1.denar2 -= int(velikost) * igra1.matrika[igra1.vrstica][igra1.stolpec]
     igra1.denar1 += int(velikost) * igra1.matrika[igra1.vrstica][igra1.stolpec]
     mon = igra1.matrika[igra1.vrstica][igra1.stolpec]
-    return bottle.template("igrisce1.html", ime1=ime1, ime2=ime2, velikost=int(velikost), mon=mon, igra=igra1)
+    return bottle.template("igrisce1.html", ime1=ime1, ime2=ime2, velikost=int(velikost), mon=mon, igra=igra1, jezik=jezik)
 
 @bottle.get("/gibanica1/")
 def gibanje21():
@@ -106,7 +116,7 @@ def gibanje21():
     igra1.denar2 -= int(velikost) * igra1.matrika[igra1.vrstica][igra1.stolpec]
     igra1.denar1 += int(velikost) * igra1.matrika[igra1.vrstica][igra1.stolpec]
     mon = igra1.matrika[igra1.vrstica][igra1.stolpec]
-    return bottle.template("igrisce1.html", ime1=ime1, ime2=ime2, velikost=int(velikost), mon=mon, igra=igra1)
+    return bottle.template("igrisce1.html", ime1=ime1, ime2=ime2, velikost=int(velikost), mon=mon, igra=igra1, jezik=jezik)
 
 @bottle.get("/igralnica2/")
 def gibanje12():
@@ -114,7 +124,7 @@ def gibanje12():
     igra2.denar2 -= int(velikost) * igra2.matrika[igra2.vrstica][igra2.stolpec]
     igra2.denar1 += int(velikost) * igra2.matrika[igra2.vrstica][igra2.stolpec]
     mon = igra2.matrika[igra2.vrstica][igra2.stolpec]
-    return bottle.template("igrisce2.html", ime1=ime1, ime2=ime2, velikost=int(velikost), mon=mon, igra=igra2)
+    return bottle.template("igrisce2.html", ime1=ime1, ime2=ime2, velikost=int(velikost), mon=mon, igra=igra2, jezik=jezik)
 
 @bottle.get("/gibanica2/")
 def gibanje22():
@@ -123,10 +133,25 @@ def gibanje22():
     igra2.denar2 -= int(velikost) * igra2.matrika[igra2.vrstica][igra2.stolpec]
     igra2.denar1 += int(velikost) * igra2.matrika[igra2.vrstica][igra2.stolpec]
     mon = igra2.matrika[igra2.vrstica][igra2.stolpec]
-    return bottle.template("igrisce2.html", ime1=ime1, ime2=ime2, velikost=int(velikost), mon=mon, igra=igra2)
+    return bottle.template("igrisce2.html", ime1=ime1, ime2=ime2, velikost=int(velikost), mon=mon, igra=igra2, jezik=jezik)
 
 @bottle.get("/navodila/")
 def navodila():
-    return bottle.template("navodila.html")
+    return bottle.template("navodila.html", jezik=jezik)
+
+@bottle.get("/primer/")
+def primer():
+    global primer
+    primer = Igra(3, 100, 100, [(0, 0, 20), (1, 1, 20), (2, 1, 20)])
+    primer.polog()
+    primer.stolpec = 0
+    return bottle.template("primer.html", primer=primer, jezik=jezik)
+
+@bottle.get("/primer1/")
+def primer1():
+    niz = bottle.request.query["premik"]
+    primer.korak(niz)
+    return bottle.template("primer.html", primer=primer, jezik=jezik)
+
 bottle.run(reloader=True)
 
